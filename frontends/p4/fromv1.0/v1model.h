735d15d25fea56b1342f7c8188121015cca72360
Thomas Holterbach <thomasholterbach@gmail.com>
2017-07-21 12:37:30 +0200

    fix.

--- frontends/p4/fromv1.0/v1model.h
+++ frontends/p4/fromv1.0/v1model.h
@@ -199,7 +199,7 @@ struct Algorithm_Model : public ::Model::Enum_Model {
     Algorithm_Model() : ::Model::Enum_Model("HashAlgorithm"),
                         crc32("crc32"), crc32_custom("crc32_custom"),
                         crc16("crc16"), crc16_custom("crc16_custom"),
-                        random("random"), identity("identity"), xxh64('xxh64') {}
+                        random("random"), identity("identity"), xxh64("xxh64") {}
     ::Model::Elem crc32;
     ::Model::Elem crc32_custom;
     ::Model::Elem crc16;
